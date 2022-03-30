b772fe61cbd11e3015a6542555fea8b76cc46dc0
Adityanarayan Tiwari <adityant17121997@gmail.com>
2022-03-30 10:27:09 +0530

    React Hook form with Controller and Material UI DatePicker

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TextField from "@mui/material/TextField";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import DesktopDatePicker from "@mui/lab/DesktopDatePicker";
+// date-fns
+import DateAdapter from "@mui/lab/AdapterDateFns";
+import {
+  useForm,
+  Controller,
+  SubmitHandler,
+} from "react-hook-form";
+import { Button, Stack } from "@mui/material";
+
+type FormValues = {
+  username: string;
+  email: string;
+  dateOfBirth: Date | null;
+};
 
 function App() {
+  const { handleSubmit, control,getValues } = useForm<FormValues>({
+    defaultValues: {
+      username: "",
+      email: "",
+      dateOfBirth: null,
+    },
+  });
+  
+  const onSubmit: SubmitHandler<FormValues> = (data: FormValues) => {
+    console.log(data);
+    // setUserData(dirtyFields)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{ padding: "4rem", width: "40rem", margin: "0 auto" }}
+    >
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Stack spacing={3}>
+          <Controller
+            control={control}
+            name="username"
+            render={({ field }) => (
+              <TextField
+                {...field}
+                id="outlined-basic"
+                label="Username"
+                variant="outlined"
+                size="small"
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="email"
+            render={({ field }) => (
+              <TextField
+                {...field}
+                id="outlined-basic"
+                label="Email"
+                variant="outlined"
+                size="small"
+              />
+            )}
+          />
+          <LocalizationProvider dateAdapter={DateAdapter}>
+            <Controller
+              control={control}
+              name="dateOfBirth"
+              render={({ field: { onChange, onBlur, value, ref } }) => (
+                <DesktopDatePicker
+                  label="Date Of Birth"
+                  inputFormat="MM/dd/yyyy"
+                  value={value}
+                  onChange={onChange}
+                  renderInput={(params) => (
+                    <TextField variant="outlined" size="small" {...params} />
+                  )}
+                />
+              )}
+            />
+          </LocalizationProvider>
+          <Button variant="contained" type="submit" onClick={()=>{
+             alert(JSON.stringify(getValues(['username','email','dateOfBirth'])))
+          }}>
+            Submit
+          </Button>
+        </Stack>
+      </form>
     </div>
   );
 }
